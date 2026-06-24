4a7fa3e35aafca6aa6e2ce5f1591343c268d5ff4
vardansunku-crypto <vardansunku@gmail.com>
2026-06-24 10:48:05 +0530

    Update cleaner.py

--- utils/cleaner.py
+++ utils/cleaner.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 def clean_dataframe_columns(dataframe):
     dataframe = dataframe.copy()
     dataframe.columns = dataframe.columns.astype(str)
@@ -7,6 +10,13 @@ def clean_dataframe_columns(dataframe):
     dataframe = dataframe.loc[:, ~dataframe.columns.str.contains("^Unnamed")]
     dataframe = dataframe.loc[:, ~dataframe.columns.duplicated()]
     dataframe = dataframe.dropna(axis=1, how="all")
+
+    # Convert list/dict columns to string for MongoDB compatibility
+    for column in dataframe.columns:
+        dataframe[column] = dataframe[column].apply(
+            lambda x: str(x) if isinstance(x, (list, dict)) else x
+        )
+
     dataframe = dataframe.drop_duplicates()
 
     return dataframe
